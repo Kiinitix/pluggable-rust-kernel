742132ae1758c0eb7853670c76e9d340bd0252c9
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2024-07-24 21:41:24 +0530

    Set up project for no_std environment and added support for x86_64-unknown-none target
    - Updated .cargo/config.toml to configure build for x86_64-unknown-none target
    - Added dependencies and panic configurations in Cargo.toml
    - Configured src/main.rs for no_std and no_main
    - Ensured compatibility with the latest nightly Rust toolchain

---
+++ velox/target/debug/build/bootloader-c10d93fb62034317/build_script_build-c10d93fb62034317.d
@@ -0,0 +1,5 @@
+/home/kabir/Desktop/pluggable-rust-kernel/velox/target/debug/build/bootloader-c10d93fb62034317/build_script_build-c10d93fb62034317: /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/build.rs
+
+/home/kabir/Desktop/pluggable-rust-kernel/velox/target/debug/build/bootloader-c10d93fb62034317/build_script_build-c10d93fb62034317.d: /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/build.rs
+
+/home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/build.rs:
---
+++ velox/target/x86_64-velox/debug/deps/bootloader-bcb06e2a2e473318.d
@@ -0,0 +1,9 @@
+/home/kabir/Desktop/pluggable-rust-kernel/velox/target/x86_64-velox/debug/deps/bootloader-bcb06e2a2e473318.rmeta: /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/lib.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/mod.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/memory_map.rs
+
+/home/kabir/Desktop/pluggable-rust-kernel/velox/target/x86_64-velox/debug/deps/libbootloader-bcb06e2a2e473318.rlib: /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/lib.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/mod.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/memory_map.rs
+
+/home/kabir/Desktop/pluggable-rust-kernel/velox/target/x86_64-velox/debug/deps/bootloader-bcb06e2a2e473318.d: /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/lib.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/mod.rs /home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/memory_map.rs
+
+/home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/lib.rs:
+/home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/mod.rs:
+/home/kabir/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.29/src/bootinfo/memory_map.rs:
